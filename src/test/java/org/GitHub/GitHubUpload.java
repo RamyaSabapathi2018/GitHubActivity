4024ca17a02f552aa2f577121ac74e4f56c62673
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-09 10:31:34 +0530

    Upload in Master branch

---
+++ src/test/java/org/GitHub/GitHubUpload.java
@@ -0,0 +1,11 @@
+package org.GitHub;
+
+public class GitHubUpload {
+	
+	public static void main(String[] args) {
+		System.out.println("To Upload in Master branch");
+		int a=10;
+		System.out.println("Add 10"+ (a+10));
+	}
+
+}
