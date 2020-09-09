73ca614a10fd1810bbbbc1c981d3a2e01f1f22e0
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-09 10:54:45 +0530

    added to sample2

--- src/test/java/org/GitHub/GitHubUpload.java
+++ src/test/java/org/GitHub/GitHubUpload.java
@@ -4,7 +4,11 @@ public class GitHubUpload {
 	
 	public static void main(String[] args) {
 		System.out.println("To Upload in Master branch");
+		String s2 = "Conflict Check";
 		int a=10;
+		System.out.println(s2.indexOf('c'));
+		System.out.println(s2.indexOf('e'));
+		System.out.println(s2.substring(2));
 		System.out.println("Add 10"+ (a+10));
 	}
 
